37d285b78bb19c89065477daf03ea7739b123158
Paul Kry <kry@cs.mcgill.ca>
2023-01-14 12:37:23 -0500

    nuked some silly/confusing provided code

--- COMP559A1/src/Midpoint.hpp
+++ COMP559A1/src/Midpoint.hpp
@@ -13,14 +13,7 @@ public:
     void step(VectorXf& p, int n, float t, float h, VectorXf& pout, Function* derivs) {
         // TODO: Objective 4, implement midpoint method
 
-        // You will probably want a temporary array in this method and perhaps
-        // multiple temporary arrays in other higher order explicit integrators.
-        // Avoid thrashing memory by reallocating only when necessary.
-        if (tmp == NULL || tmplength != n) {
-            if (tmp != NULL) delete tmp;
-            tmp = new float[n];
-            tmplength = n;
-        }
+        // You will probably want a temporary array in this method 
 
     }
 
